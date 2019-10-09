25c86f529aa8390691ddcf53f545a3ad82af958b
Warren Vella <warren.vella18@gmail.com>
2019-10-10 07:19:54 +1100

    Moved the githubClientSecret out of the file so it is not stored in GIT. Update the local port.

--- sample-app/index.php
+++ sample-app/index.php
@@ -20,9 +20,17 @@ function apiRequest($url, $post=FALSE, $headers=array()) {
   return json_decode($response, true);
 }
 
+//The $githubClientSecret variable is stored in the client-secret.php file. This is so this file is not uploaded
+//to the public GIT repo. If you are building this simple web application locally, you need to create a a file
+//with the following contents:
+
+/*<?php
+//$githubClientSecret = '<<yoursecret>>';
+?>*/
+include 'client-secret.php';
+
 // Fill these out with the values from Github
 $githubClientID = '7c4d14bbba5ef0b6d4b1';
-$githubClientSecret = '003a071bb8d98eb796d84d365b1fe5b741e7a9a6';
 
 // This is the URL we'll send the user to first
 // to get their authorization
@@ -35,8 +43,8 @@ $tokenURL = 'https://github.com/login/oauth/access_token';
 $apiURLBase = 'https://api.github.com/';
 
 // The URL for this script, used as the redirect URL
-$baseURL = 'http://' . $_SERVER['SERVER_NAME']
-    . $_SERVER['PHP_SELF'];
+$baseURL = 'http://' . $_SERVER['SERVER_NAME'] . ':8000'
+   . $_SERVER['PHP_SELF'];
 
     // Start a session so we have a place to
 // store things between redirects
